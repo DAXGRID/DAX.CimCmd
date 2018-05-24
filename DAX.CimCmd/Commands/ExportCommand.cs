64d0a8b15da72f2eb111eff50278eaccdc1e9610
Jesper Ladegaard <jesper@dax.dk>
2018-05-24 09:03:52 +0200

    DAX.IO and DAX.CIM adapter stuff added, so that GIS data can be mapped to CIM (CSON).

--- DAX.CimCmd/Commands/ExportCommand.cs
+++ DAX.CimCmd/Commands/ExportCommand.cs
@@ -1,29 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using DAX.CIM.PhysicalNetworkModel;
+using DAX.Cson;
+using DAX.IO;
+using DAX.IO.CIM;
+using DAX.IO.CIM.DataModel;
+using DAX.IO.CIM.Processing;
+using DAX.IO.CIM.Serialize;
+using DAX.IO.Writers;
 using GoCommando;
+using Serilog.Core;
 
 namespace DAX.CimCmd.Commands
 {
     [Command("export")]
-    [Description("Generates an export")]
+    [Description("Generates an full CIM export")]
     public class ExportCommand : BaseCommandWithOutputFile
     {
-        [Parameter("from")]
-        [Description("Specifies which file to use as the starting point")]
-        public string FromFilePath { get; set; }
-
-        [Parameter("diff")]
-        [Description("Specifies which diff file to apply")]
-        public string DiffFilePath { get; set; }
-
+        [Parameter("configFile")]
+        [Description("Specifies which CIM Adapter config to use")]
+        public string ConfigFileName { get; set; }
+                     
         protected override async Task Execute()
         {
             await base.Execute();
 
-            AssertFileExists(FromFilePath);
-            AssertFileExists(DiffFilePath);
+            AssertFileExists(ConfigFileName);
+         
+            Console.WriteLine("Creating full CIM network dump based on configuration: " + ConfigFileName);
 
-            Console.WriteLine("Do full export in here ");
+            var config = new TransformationConfig().LoadFromFile(ConfigFileName);
+
+            //config.DataReaders[0].ConfigParameters.Add(new ConfigParameter { Name = "Extent", Value = "548694,552348,6187246,6189391" });
+
+            var transformer = config.InitializeDataTransformer("test");
+            ((CIMGraphWriter)transformer.GetFirstDataWriter()).DoNotRunPreCheckConnectivity();
+                    
+            transformer.TransferData();
+                   
+            var writer = (CIMGraphWriter)transformer.GetFirstDataWriter();
+            var graph = writer.GetCIMGraph();
+
+            var stopWatch = Stopwatch.StartNew();
+                        
+            var allCimObjects = GetIdentifiedObjects(config, graph, true, true, true).ToList();
+
+            var cson = new CsonSerializer();
+
+            using (var destination = File.Open(base.ExportFilePath, FileMode.Create))
+            {
+                using (var source = cson.SerializeObjects(allCimObjects))
+                {
+                    source.CopyTo(destination);
+                }
+            }
+
+
+
+        }
+
+        static IEnumerable<IdentifiedObject> GetIdentifiedObjects(TransformationConfig config, CIMGraph graph, bool includeEquipment = false, bool includeAssets = false, bool includeLocations = false)
+        {
+            var serializer = config.InitializeSerializer("DAX") as IDAXSerializeable;
+            var daxcimSerializer = (DAXCIMSerializer)serializer;
+            var dataRepository = CIMMetaDataManager.Repository;
+            var objects = graph.CIMObjects;
+
+            return daxcimSerializer.GetIdentifiedObjects(dataRepository, objects, includeEquipment, includeAssets, includeLocations);
         }
     }
 }
